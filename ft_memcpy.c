360748a6a8738bf0df60cace489152095ad6198d
Boris Gazur <barlukh@gmail.com>
2025-04-16 14:58:17 +0300

    ft_memcpy final

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,25 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:01:50 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:01:51 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/16 14:56:41 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*ptrd;
+	const unsigned char	*ptrs;
+	size_t				i;
+
+	ptrd = (unsigned char *)dest;
+	ptrs = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		ptrd[i] = ptrs[i];
+		i++;
+	}
+	return (dest);
+}
