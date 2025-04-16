ab534dbcecd4d5b92019dfc59b758d7e910c5ad7
Boris Gazur <barlukh@gmail.com>
2025-04-16 13:17:15 +0300

    ft_bzero final

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,19 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:01:22 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:01:23 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/16 13:11:37 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+		ptr[i++] = '\0';
+}
