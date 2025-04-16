a02d9c4f466cda9131187708953c8a84c047d358
Boris Gazur <barlukh@gmail.com>
2025-04-16 12:06:53 +0300

    ft_memset final

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,22 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:00:48 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:00:49 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/16 12:04:42 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	uchar;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	uchar = (unsigned char)c;
+	i = 0;
+	while (i < n)
+		ptr[i++] = uchar;
+	return (s);
+}
