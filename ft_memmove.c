9138bae00487dfcf46cdd6d12048948abbac0f18
Boris Gazur <barlukh@gmail.com>
2025-04-16 17:50:28 +0300

    ft_memmove final

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,37 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:02:09 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:02:10 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/16 17:49:45 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*ptrd;
+	const unsigned char	*ptrs;
+	size_t				i;
+
+	ptrd = (unsigned char *)dest;
+	ptrs = (const unsigned char *)src;
+	if (ptrs < ptrd)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			ptrd[i] = ptrs[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			ptrd[i] = ptrs[i];
+			i++;
+		}
+	}
+	return (dest);
+}
