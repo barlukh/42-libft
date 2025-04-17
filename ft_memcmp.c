8523c8eabd7f447a99922d1421645e110e104552
Boris Gazur <barlukh@gmail.com>
2025-04-17 17:51:06 +0300

    ft_memcmp final

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,26 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:06:12 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:06:15 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/17 17:49:48 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptrs1;
+	const unsigned char	*ptrs2;
+	size_t				i;
+
+	ptrs1 = (const unsigned char *)s1;
+	ptrs2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (ptrs1[i] != ptrs2[i])
+			return (ptrs1[i] - ptrs2[i]);
+		i++;
+	}
+	return (0);
+}
