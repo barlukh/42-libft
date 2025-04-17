24285aa6eac131356a0f696e77d3d477e6649b28
Boris Gazur <barlukh@gmail.com>
2025-04-17 10:17:25 +0300

    ft_strrchr final

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,28 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:04:32 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:04:33 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/17 10:16:20 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*ptr;
+	char	*result;
+	size_t	i;
+
+	ptr = (char *)s;
+	result = NULL;
+	i = 0;
+	while (ptr[i] != '\0')
+	{
+		if (ptr[i] == c)
+			result = &ptr[i];
+		i++;
+	}
+	if (ptr[i] == c)
+		result = &ptr[i];
+	return (result);
+}
