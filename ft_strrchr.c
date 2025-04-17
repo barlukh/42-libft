5d7fa526a981dec6d2f1a6458a36a84f33ec12a2
Boris Gazur <barlukh@gmail.com>
2025-04-17 15:54:24 +0300

    Change variable name

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:04:32 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/17 14:52:20 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/17 15:53:44 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	const char	*result;
+	const char	*last;
 	size_t		i;
 
-	result = NULL;
+	last = NULL;
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == (unsigned char)c)
-			result = &s[i];
+			last = &s[i];
 		i++;
 	}
 	if ((unsigned char)c == '\0')
 		return ((char *)&s[i]);
-	return ((char *)result);
+	return ((char *)last);
 }
