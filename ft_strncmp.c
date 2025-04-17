672245b0c520ad73dfa804c6f4cefa5d106a98d7
Boris Gazur <barlukh@gmail.com>
2025-04-17 15:36:59 +0300

    Shorten code with typecasting

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:05:08 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/17 11:34:49 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/17 15:35:33 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	c1;
-	unsigned char	c2;
 
 	i = 0;
 	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		c1 = (unsigned char)s1[i];
-		c2 = (unsigned char)s2[i];
-		if (c1 != c2)
-			return (c1 - c2);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
