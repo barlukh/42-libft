56a40e2537f943a89cdadbbe0815bf8355ef67fa
Boris Gazur <barlukh@gmail.com>
2025-04-17 11:35:49 +0300

    ft_strncmp final

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,26 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:05:08 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:05:09 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/17 11:34:49 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	c1;
+	unsigned char	c2;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		c1 = (unsigned char)s1[i];
+		c2 = (unsigned char)s2[i];
+		if (c1 != c2)
+			return (c1 - c2);
+		i++;
+	}
+	return (0);
+}
