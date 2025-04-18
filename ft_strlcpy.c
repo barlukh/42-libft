646d67c90d54bbae0b20198915705646657a0911
Boris Gazur <barlukh@gmail.com>
2025-04-18 09:35:27 +0300

    ft_strlcpy final

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,24 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:02:36 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:02:37 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/18 09:34:42 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (size == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
