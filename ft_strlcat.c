cdafced3088b5ec4639709064283af487cea9def
Boris Gazur <barlukh@gmail.com>
2025-04-18 12:13:34 +0300

    ft_strlcat final

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,28 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:03:05 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:03:07 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/18 12:12:42 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (dst_len + src_len);
+	i = 0;
+	while (src[i] != '\0' && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
