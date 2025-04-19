09ea11e854c12645b9a232cba523a9adb55fb955
Boris Gazur <barlukh@gmail.com>
2025-04-19 11:36:44 +0300

    ft_atoi final

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,40 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:07:12 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:07:13 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/19 11:35:30 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	if ((c >= '\t' && c <= '\r') || c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	long long int	sign;
+	long long int	result;
+	size_t			i;	
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] != '\0' && ft_isdigit(nptr[i]))
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return ((int)(result * sign));
+}
