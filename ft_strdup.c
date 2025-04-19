6d0e645546ab6d499a1aa8dfd66157dc7d63524c
Boris Gazur <barlukh@gmail.com>
2025-04-19 13:11:10 +0300

    ft_strdup final

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,28 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:08:23 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/15 11:08:24 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/19 13:02:29 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	s_len;
+	char	*ptr;
+	size_t	i;
+
+	s_len = ft_strlen(s);
+	ptr = malloc(sizeof(char) * (s_len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
