6a7f1b08333b5be273ae67512abe0e3c90d44bd1
Boris Gazur <barlukh@gmail.com>
2025-04-21 09:51:02 +0300

    Typecast start into size_t

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 07:13:13 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/21 09:27:34 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/21 09:48:57 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	s_len;
+	size_t	start_i;
 	size_t	ss_len;
 	char	*ss;
 	size_t	i;
@@ -39,18 +40,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
-	if (start >= s_len)
+	start_i = (size_t)start;
+	if (start_i >= s_len)
 		return (ft_strdup(""));
-	else if (s_len - start >= len)
+	else if (s_len - start_i >= len)
 		ss_len = len;
 	else
-		ss_len = s_len - start;
+		ss_len = s_len - start_i;
 	ss = malloc(sizeof(char) * ss_len + 1);
 	if (!ss)
 		return (NULL);
 	i = 0;
-	while (s[start] != '\0' && i < len)
-		ss[i++] = s[start++];
+	while (s[start_i] != '\0' && i < len)
+		ss[i++] = s[start_i++];
 	ss[i] = '\0';
 	return (ss);
 }
