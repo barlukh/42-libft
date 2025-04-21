847dea4152f2798b05f5434a07ea4c6f2fd71647
Boris Gazur <barlukh@gmail.com>
2025-04-21 14:49:34 +0300

    Initial commit

---
+++ ft_split.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 14:38:11 by bgazur            #+#    #+#             */
+/*   Updated: 2025/04/21 14:44:20 by bgazur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Allocates memory (using malloc(3)) and returns an array of strings obtained
+ * by splitting ’s’ using the character ’c’ as a delimiter. The array must
+ * end with a NULL pointer.
+ * 
+ * Functionality:
+ * 
+ * 
+ * Return value:
+ * Returns the array of new strings resulting from the split.
+ * NULL if the allocation fails. */
+
+#include "libft.h"
+
+char	**ft_split(char const *s, char c)
+{
+	
+}
