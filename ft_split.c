a667573edf0ecc2fc8e8b3fb732e13c0dc0b3ff9
Boris Gazur <barlukh@gmail.com>
2025-04-22 09:40:28 +0300

    ft_spit final

--- ft_split.c
+++ ft_split.c
@@ -6,23 +6,26 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:38:11 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/21 17:32:46 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/22 09:40:01 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Allocates memory (using malloc(3)) and returns an array of strings obtained
+/* Allocates memory (using malloc()) and returns an array of strings obtained
  * by splitting ’s’ using the character ’c’ as a delimiter. The array must
  * end with a NULL pointer.
  * 
  * Functionality:
- * 
+ * Counts the words, therefore counts the number of subarrays to malloc() for.
+ * Uses pointer arithmetic together with the ft_strchr() to find word lenghts.
+ * Correct pointer positions with word lenghts are then sent to ft_substr().
+ * Memory allocation fails are handled through ft_free_array().
+ * Last subarray is NULL-terminated.
  * 
  * Return value:
  * Returns the array of new strings resulting from the split.
  * NULL if the allocation fails. */
 
 #include "libft.h"
-#include <stdio.h>
 
 static size_t	ft_word_count(char const *s, char c)
 {
@@ -34,55 +37,64 @@ static size_t	ft_word_count(char const *s, char c)
 		while (*s == c)
 			s++;
 		if (*s)
+		{
 			count++;
-		while (*s != c && *s)
-			s++;
+			while (*s && *s != c)
+				s++;
+		}
 	}
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+static void	ft_free_array(char **arr, size_t i)
 {
-	char	**arr;
+	while (i > 0)
+		free(arr[i--]);
+	free(arr[i]);
+	free(arr);
+}
+
+static char	**ft_allocate_array(char const *s, char c, char **arr)
+{
 	size_t	word_len;
 	size_t	i;
 
-	if (!s || !c)
-		return (NULL);
-	arr = malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
-		return (NULL);
 	i = 0;
 	while (*s)
 	{
-		while (*s == c && *s)
+		while (*s == c)
 			s++;
 		if (*s)
 		{
-			if (!ft_strchr(s, c))
-				word_len = ft_strlen(s);
-			else
+			if (ft_strchr(s, c))
 				word_len = ft_strchr(s, c) - s;
-			arr[i++] = ft_substr(s, 0, word_len);
+			else
+				word_len = ft_strlen(s);
+			arr[i] = ft_substr(s, 0, word_len);
+			if (!arr[i])
+			{
+				ft_free_array(arr, i);
+				return (NULL);
+			}
 			s += word_len;
+			i++;
 		}
 	}
-	arr[i] = NULL;
 	return (arr);
 }
 
-int main(void)
+char	**ft_split(char const *s, char c)
 {
-	char	*s = "Hello my name is Boris.";
-	char	**arr = ft_split(s, ' ');
-	size_t	i = 0;
+	size_t	word_count;
+	char	**arr;
 
-	while(arr[i] != NULL)
-	{
-		printf("%s\n", arr[i]);
-		i++;
-	}
-
-	free(arr);
-
-	return (0);
+	if (!s)
+		return (NULL);
+	word_count = ft_word_count(s, c);
+	arr = malloc(sizeof(char *) * (word_count + 1));
+	if (!arr)
+		return (NULL);
+	arr = ft_allocate_array(s, c, arr);
+	arr[word_count] = NULL;
+	return (arr);
 }
