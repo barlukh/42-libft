a71f6c9685d8ce8e103d7f474062366461ceccd3
Boris Gazur <barlukh@gmail.com>
2025-04-24 15:30:32 +0300

    Fix returnn value

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:03:05 by bgazur            #+#    #+#             */
-/*   Updated: 2025/04/20 15:11:18 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/04/24 15:30:06 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@
  * 
  * Return value:
  * Returns the total length of the string it tried to create, that means 
- * the initial length of 'dst' plus the length of 'src'. */
+ * the initial length of 'dst' plus the length of 'src'.
+ * Note that if 'size' <= 'dst', return is 'size' + length of 'src'. */
 
 #include "libft.h"
 
@@ -35,7 +36,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	if (size <= dst_len)
-		return (dst_len + src_len);
+		return (size + src_len);
 	i = 0;
 	while (src[i] != '\0' && dst_len + i < size - 1)
 	{
