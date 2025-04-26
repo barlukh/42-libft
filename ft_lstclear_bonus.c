706d3dce7f5b896bfb60c42348de5e0a707ed571
Boris Gazur <barlukh@gmail.com>
2025-04-26 10:35:50 +0300

    Initial commit

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 07:20:18 by bgazur            #+#    #+#             */
+/*   Updated: 2025/04/26 08:18:59 by bgazur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst != NULL)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 08:58:34 by bgazur            #+#    #+#             */
+/*   Updated: 2025/04/26 10:32:32 by bgazur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_head;
+	t_list	*new_node;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_head = NULL;
+	while (lst != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+		{
+			ft_lstclear(&new_node, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_head, new_node);
+		lst = lst->next;
+	}
+	return (new_head);
+}
